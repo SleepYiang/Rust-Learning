238111593cfee7e1c70b74878fd1a0f7a3c94cf7
sleepyiang <yiang_1970@163.com>
2026-05-22 16:52:37 +0800

    猜字游戏完成

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,34 @@
 use std::io;
-
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
+
+    let secret_number=rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
    //创建一个可变的变量 guess
-   let mut guess=String::new();
+   let mut guess =String::new();
 
     io::stdin()
-        .read_line(&mut guess)//打
-        .expect("Failed to read line");
+        .read_line(&mut guess)
+        .expect("Failed to read line");//防止程序出现崩溃
+    let guess: u32=guess.trim().parse().expect("Please type a number!");//将字符串转换为数字
+    loop{
+        println!("You guessed: {}",guess);
 
-    println!("You guessed: {guess}");
-
-    let x=5;
-    let y=10;
-    println!("x={x},y+2={}",y+2);
+        match guess.cmp(&secret_number){
+        Ordering::Less=>println!("Too small!"),
+        Ordering::Greater=>println!("Too big!"),
+        Ordering::Equal=>{
+            println!("You win!"),
+            break;
+            }
+        }
+    }
+    // let x=5;
+    // let y=10;
+    // println!("x={x},y+2={}",y+2);
 }
