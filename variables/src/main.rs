6a0429ed5daee4b6ae2e87f3118338b6ebcc0fc5
sleepyiang <yiang_1970@163.com>
2026-05-23 18:04:38 +0800

    变量更新

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;//mut 修饰可变的变量内容
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    //定义常量
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds is: {}", THREE_HOURS_IN_SECONDS);
+
+    //rust中内容的遮蔽
+    let v = 5;
+
+    let v = v + 1;
+
+    {
+        let v = v * 2;
+        println!("The value of v in the inner scope is: {}", v);
+    }
+
+    println!("The value of v is: {}", v);
+
+    //遮蔽可以同名不同类型
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+
+
 }
