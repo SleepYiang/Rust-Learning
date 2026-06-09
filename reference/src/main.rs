048c097a57252ccf1f082e0938833ee72a9a20eb
sleepyiang <yiang_1970@163.com>
2026-06-09 22:32:12 +0800

    代码更新

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -15,9 +15,16 @@ fn main()
 {
     let mut s2=String::from("hello");
     change(& mut s2);
-     println!("The length of '{}' is {}",s2,s2);
+    println!("The length of '{}' is {}",s2,s2);
+    let reference_to_nothing = denger();
 }
 fn change(some_string:&mut String)
 {
     some_string.push_str(", world");
+}
+
+//悬垂引用（不可以返回引用类型）
+fn denger() ->String{
+    let s = String::from("hello");
+    s
 }
