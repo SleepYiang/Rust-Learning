aba1c204ed8e099f5f3329e46aec8cd086e0d16f
sleepyiang <yiang_1970@163.com>
2026-06-12 21:53:22 +0800

    test

--- enmu/src/main.rs
+++ enmu/src/main.rs
@@ -1,14 +1,17 @@
 //枚举类型的相关内容
-enum IpAdressKey{
-    V4,
-    V6,
-}
-//关联结构体使用
-struct IPadd{
-    kind:IpAdressKey,
-    add:String,
+// enum IpAdressKey{
+//     V4,
+//     V6,
+// }
 
-}
+
+
+//关联结构体使用
+// struct IPadd{
+//     kind:IpAdressKey,
+//     add:String,
+
+// }
 //使用枚举变量（创建实例）
 // let four=IpAdressKey::V4;
 // let six=IpAdressKey::V6;
@@ -18,24 +21,42 @@ struct IPadd{
 //     add:String,
 // }
 
-fn router(ip: IPadd) {
-    match ip.kind {
-        IpAdressKey::V4 => println!("路由IPv4地址: {}", ip.add),
-        IpAdressKey::V6 => println!("路由IPv6地址: {}", ip.add),
-    }
+// fn router(ip: IPadd) {
+//     match ip.kind {
+//         IpAdressKey::V4 => println!("路由IPv4地址: {}", ip.add),
+//         IpAdressKey::V6 => println!("路由IPv6地址: {}", ip.add),
+//     }
+// }
+
+// fn main()
+// {
+//     let ip1=IPadd{
+//         kind:IpAdressKey::V4,
+//         add:String::from("127.0.0.1"),
+//     };
+
+//     let ip2=IPadd {
+//          kind:IpAdressKey::V6,
+//           add:String::from("::1"),
+//         };
+
+//     router(ip1);
+//     router(ip2);
+// }
+
+
+//第二种枚举实现
+#[derive(Debug)]
+enum IpAdressKey{
+    V4(String),
+    V6(String),
 }
+
 fn main()
 {
-    let ip1=IPadd{
-        kind:IpAdressKey::V4,
-        add:String::from("127.0.0.1"),
-    };
-
-    let ip2=IPadd {
-         kind:IpAdressKey::V6,
-          add:String::from("::1"),
-        };
-
-    router(ip1);
-    router(ip2);
+   let home=IpAdressKey::V4(String::from("127.0.0.1"));
+   let loop_CallBack=IpAdressKey::V6(String::from("::1"));
+   println!("{:?}",home);
+   println!("{:?}",loop_CallBack)
 }
+//test
