f457cf91b102aebdba99b238472e4030e143c651
sleepyiang <yiang_1970@163.com>
2026-06-13 19:36:08 +0800

    match进阶理解

--- match_st/src/main.rs
+++ match_st/src/main.rs
@@ -1,4 +1,5 @@
 //match学习
+#[derive(Debug)]
 enum Coin{
     Penny,
     Nickel,
@@ -11,5 +12,32 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
+    }
+}
+
+fn cents_in_value(value:u8)->Option<Coin>{
+    match value{
+        1=>Some(Coin::Penny),
+        5=>Some(Coin::Nickel),
+        10=>Some(Coin::Dime),
+        25=>Some(Coin::Quarter),
+        _ =>None,
+    }
+}
+
+
+fn main()
+{
+    let x=Coin::Penny;
+    let y=value_in_cents(x);
+    println!("result{}",y);
+    let z:u8=5;
+    let resylt=cents_in_value(z);
+    //println!("result1{}",resylt);//错误使用方法枚举类型不可以直接打印
+    println!("result1{:?}",resylt);//配合debug模式下的：？完成打印
+    //方法二
+    match cents_in_value(z){
+        Some(coin)=>println!("result2{}",value_in_cents(coin)),
+        None=>println!("无效字符"),
     }
 }
