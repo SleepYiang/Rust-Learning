30b267e452255f80e1f33478023a6edd8a97f171
sleepyiang <yiang_1970@163.com>
2026-07-01 15:54:47 +0800

    闭包更新

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,9 +1,15 @@
 //闭包
 fn main() {
+    use std::mem;
    let x = 1;
    let sum = |y| x + y;   //采用值捕获的方式捕获x的值 //sum中传二则为2
 
     assert_eq!(3, sum(2));
+
+    let color = String::from("green");
+    let print=|| println!("`color`: {}", color);
+    print();
+
 }
 
 //代码更新
